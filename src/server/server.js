6762e9b46648d0200960a14c4ef027c51cf4b5fb
alpiy <alfiahmad327@gmail.com>
2024-06-16 23:53:32 +0700

    Deploy using Cloud Run

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ const InputError = require('../exceptions/InputError');
 
 (async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT,
         host: '0.0.0.0',
         routes: {
             cors: {
